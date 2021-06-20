72262a0d0c5cb77255c2de294296c565b5dd396b
Ken Takahashi <ken.t.csai@gmail.com>
2021-06-20 22:24:06 +0900

    init

---
+++ CountUpApp/CountUpApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CountUpApp
+//
+//  Created by Ken Takahashi on 2021/06/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
